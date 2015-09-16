73c3ad99992b6092af7c28221a2995f28dda0be1
jlboulanger <jl_boul@yahoo.fr>
2015-09-17 00:15:51 +0200

    test on form

--- app/controllers/SecondPage.java
+++ app/controllers/SecondPage.java
@@ -1,14 +1,24 @@
 package controllers;
 
+import java.util.stream.Stream;
+
+import model.FormData;
 import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import play.data.Form;
 
 public class SecondPage extends Controller {
 
 
     public Result index() {
         return ok(views.html.second.index.render("new page"));
+    }
+    
+    public Result indexWithForm() {
+        Form<FormData> form = Form.form(FormData.class);
+        //TODO
+        return ok(views.html.second.index.render(form.bindFromRequest(request(), "field1").get().getField1()));
     }
 }
---
+++ app/model/FormData.java
@@ -0,0 +1,14 @@
+package model;
+
+public class FormData {
+
+    private String field1;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+}
